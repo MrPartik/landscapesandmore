96fdf9632751d8e7ee4d0e9d7008f340ea098230
John Patrick Loyola <loyolapat04@gmail.com>
2022-04-25 20:03:42 +0800

    Fix color in warranty admin counter dashboard

--- resources/views/livewire/admin/warranty.blade.php
+++ resources/views/livewire/admin/warranty.blade.php
@@ -30,14 +30,14 @@
                         <div class="config-one-item animated animation fadeInDown">
                             <a href="javascript:"><i class="fa fa-times orange"></i>&nbsp;<i class="fa fa-phone orange"></i></a>
                             <h2>{{ $aCounts['not_contacted'] }}</h2>
-                            <h4 class="br-green">Not yet contacted</h4>
+                            <h4 class="br-orange">Not yet contacted</h4>
                         </div>
                     </div>
                     <div class="col-md-2 col-xs-4">
                         <div class="config-one-item animated animation fadeInDown">
                             <a href="javascript:"><i class="fa fa-times orange"></i>&nbsp;<i class="fa fa-check orange"></i></a>
                             <h2>{{ $aCounts['not_resolved'] }}</h2>
-                            <h4 class="br-green">Not yet resolved</h4>
+                            <h4 class="br-orange">Not yet resolved</h4>
                         </div>
                     </div>
                     <div class="col-md-2 col-xs-4">
