a2b2afdaefe349a1da92d8dfa4b418b58d7993d9
John Patrick Loyola <loyolapat04@gmail.com>
2022-05-19 23:36:51 +0800

    Added chat plugin

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -74,4 +74,17 @@
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     @livewireScripts
     @yield('extra-js')
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/61bb3e93c82c976b71c1c1b0/1fn1mos74';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
 </html>
