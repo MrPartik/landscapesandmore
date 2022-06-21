6bf0dc8904c1f5a3da18e136f33f1d263e3d46d6
John Patrick Loyola <loyolapat04@gmail.com>
2022-06-21 23:47:36 +0800

    Added template for "our process"

--- resources/views/front/services/residential.blade.php
+++ resources/views/front/services/residential.blade.php
@@ -40,7 +40,6 @@
                         <h3><span class="id-color">Landscaping Install & </span> Design Services</h3>
                         Residential landscaping is more than trees and plants! A big part of creating a beautiful and functional landscape is designing and installing hardscape. Hardscape — bricks, decking, concrete, pavers, etc. — provides structure to the landscape. The right hardscape can turn your yard into an oasis that you will want to spend every evening and weekend in. We can create beautiful patios for you to host dinner parties on. Or put in retaining walls, arbors, or trellises to give yourself multiple spaces to relax and unwind.
                     </div>
-
                     <div class="col-md-4 wow fadeInUp" data-wow-delay=".2s">
                         <a class="image-popup-no-margins" href="{{ url('img/services/residential/maintenance.jpg') }}">
                             <img src="{{ url('img/services/residential/maintenance.jpg') }}" class="img-responsive" alt="">
@@ -63,6 +62,84 @@
             </div>
         </section>
         <!-- section close -->
+
+        <!-- section begin -->
+        <section id="section-steps" class="text-light">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 offset-md-3 text-center wow fadeInUp">
+                        <h1>Our Process</h1>
+                        <div class="separator"><span><i class="fa fa-circle"></i></span></div>
+                        <div class="spacer-single"></div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="de_tab tab_steps">
+                            <ul class="de_nav">
+                                <li class="active wow fadeIn" data-wow-delay="0s"><span>Meet &amp; Agree</span>
+                                    <div class="v-border"></div>
+                                </li>
+                                <li class="wow fadeIn" data-wow-delay=".4s"><span>Idea &amp; Concept</span>
+                                    <div class="v-border"></div>
+                                </li>
+                                <li class="wow fadeIn" data-wow-delay=".8s"><span>Design &amp; Create</span>
+                                    <div class="v-border"></div>
+                                </li>
+                                <li class="wow fadeIn" data-wow-delay="1.2s"><span>Build &amp; Install</span>
+                                    <div class="v-border"></div>
+                                </li>
+                            </ul>
+
+                            <div class="de_tab_content">
+
+                                <div id="tab1">
+                                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque
+                                    ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+                                    explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
+                                    sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque
+                                    porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
+                                    velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam
+                                    aliquam quaerat voluptatem. Ut enim ad minima veniam.
+                                </div>
+
+                                <div id="tab2">
+                                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque
+                                    ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+                                    explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
+                                    sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque
+                                    porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
+                                    velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam
+                                    aliquam quaerat voluptatem. Ut enim ad minima veniam.
+                                </div>
+
+                                <div id="tab3">
+                                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque
+                                    ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+                                    explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
+                                    sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque
+                                    porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
+                                    velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam
+                                    aliquam quaerat voluptatem. Ut enim ad minima veniam.
+                                </div>
+
+                                <div id="tab4">
+                                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque
+                                    ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+                                    explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
+                                    sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque
+                                    porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
+                                    velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam
+                                    aliquam quaerat voluptatem. Ut enim ad minima veniam.
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- section close -->
+
         @include('front.navigation.footer')
     </div>
 @endsection
