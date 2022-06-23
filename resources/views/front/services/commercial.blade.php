bd8796b1857f04a8d7b83b15fafa8cccc41daa40
John Patrick Loyola <loyolapat04@gmail.com>
2022-06-23 21:26:30 +0800

    Applied code changes based from discussions

--- resources/views/front/services/commercial.blade.php
+++ resources/views/front/services/commercial.blade.php
@@ -357,6 +357,18 @@
                 </div>
             </div>
         </section>
+        <section id="call-to-action" class="text-dark call-to-action padding40 text-light bg-color"aria-label="cta">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-8 col-md-7">
+                        <h3 class="size-2 no-margin">Get In Touch With Us Today</h3>
+                    </div>
+                    <div class="col-lg-4 col-md-5 text-right">
+                        <a href="/contact-us" class="btn-line-white">Contact Us Now</a>
+                    </div>
+                </div>
+            </div>
+        </section>
         <!-- section close -->
         @include('front.navigation.footer')
     </div>
--- resources/views/front/services/residential.blade.php
+++ resources/views/front/services/residential.blade.php
@@ -355,6 +355,18 @@
                     </div>
                 </div>
         </section>
+        <section id="call-to-action" class="text-dark call-to-action padding40 text-light bg-color"aria-label="cta">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-8 col-md-7">
+                        <h3 class="size-2 no-margin">Get In Touch With Us Today</h3>
+                    </div>
+                    <div class="col-lg-4 col-md-5 text-right">
+                        <a href="/contact-us" class="btn-line-white">Contact Us Now</a>
+                    </div>
+                </div>
+            </div>
+        </section>
         @include('front.navigation.footer')
     </div>
 @endsection
